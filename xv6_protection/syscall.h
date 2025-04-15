57e749bb35818527a13fb53efbcbc9cc2dba0546
Cole Carter <112132207+cdcarter21@users.noreply.github.com>
2025-04-14 20:04:28 -0500

    Add files via upload

---
+++ xv6_protection/syscall.h
@@ -0,0 +1,25 @@
+// System call numbers
+#define SYS_fork    1
+#define SYS_exec    2
+#define SYS_exit    3
+#define SYS_wait    4
+#define SYS_kill    5
+#define SYS_getpid  6
+#define SYS_sleep   7
+#define SYS_sbrk    8
+
+#define SYS_open    9
+#define SYS_read   10
+#define SYS_write  11
+#define SYS_close  12
+#define SYS_pipe   13
+#define SYS_dup    14
+
+#define SYS_fstat  15
+#define SYS_mknod  16
+#define SYS_link   17
+#define SYS_unlink 18
+#define SYS_mkdir  19
+#define SYS_chdir  20
+
+#define SYS_uptime 21
